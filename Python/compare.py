52ae71304d055263e2204975353a36377c24e2b9
CoderFrancis <mfcollegeacc@gmail.com>
2024-04-11 06:46:46 -0400

    Continuing to learn

--- Python/compare.py
+++ Python/compare.py
@@ -1,10 +1,26 @@
+#if than statmenets: if, elif, else
+
+# x = int(input("What's x? "))
+# y = int(input("What's y? "))
+
+# if x > y:
+#     print("x is greater than y")
+# elif x < y:
+#     print("x is less than y")
+# else:
+#     print("x is equal to y")
+
+# or (pre improvements)
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x > y:
-    print("x is greater than y")
-elif x < y:
-    print("x is less than y")
+if x < y or x > y:
+    print("x is not equal to y")
 else:
     print("x is equal to y")
-    
+
+# or (post improvements)
+if x == y:  #or if x != y
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
