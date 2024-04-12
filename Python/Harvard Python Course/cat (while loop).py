3f05d492ef41cd0101afa7f9f90d89c113698c93
CoderFrancis <mfcollegeacc@gmail.com>
2024-04-12 11:40:29 -0400

    Learning

--- Python/Harvard Python Course/cat (while loop).py
+++ Python/Harvard Python Course/cat (while loop).py
@@ -8,4 +8,32 @@ while i < 3:
 # while i != 0
 #   print("meow")]
 # i = i - 1
-#
+
+# also
+for _ in range(3):
+    print("dog")
+
+# continuing
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
+
+# improvement
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+
+def meow(n):
+    print
